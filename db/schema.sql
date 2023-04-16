4c20f931cbe6d605e1e843bed8652011fb6a1d03
Change Agent <keasiley@icloud.com>
2023-04-16 16:03:45 -0500

    schema created

--- db/schema.sql
+++ db/schema.sql
@@ -4,31 +4,32 @@ CREATE DATABASE employee_tracker;
 USE employee_tracker;
 
 CREATE TABLE department (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 
 );
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(8,2),
+  department_id INT,
+  PRIMARY KEY(id),
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE SET NULL
+);  
+
 CREATE TABLE employee (
-  id INT NOT NULL PRIMARY KEY,
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT,
   manager_id INT,
-  PRIMARY KEY (id),
   FOREIGN KEY (role_id) 
   REFERENCES role(id),
   FOREIGN KEY (manager_id)
-  REFERENCES manager(id)
+  REFERENCES employee(id)
   ON DELETE SET NULL
-);
-CREATE TABLE role (
-  id INT NOT NULL,
-  title VARCHAR(30) NOT NULL,
-  salary DECIMAL(8,2),
-  department_id INT,
-  PRIMARY KEY (id),
-  FOREIGN KEY (department_id)
-  REFERENCES department(id)
-  ON DELETE SET NULL
-);                          
+);                                                
