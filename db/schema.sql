c92c5b64687d2294bff843010a91499636835031
Change Agent <keasiley@icloud.com>
2023-04-16 15:12:28 -0500

    schema edited

--- db/schema.sql
+++ db/schema.sql
@@ -9,26 +9,26 @@ CREATE TABLE department (
   PRIMARY KEY (id)
 
 );
-
-CREATE TABLE role (
-  id INT NOT NULL,
-  title VARCHAR(30) NOT NULL,
-  salary DECIMAL(8,2),
-  department_id INT,
-  FOREIGN KEY (department_id)
-  REFERENCES department_id(id)
-  ON DELETE SET NULL
-);                                                                             
-
 CREATE TABLE employee (
   id INT NOT NULL PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT,
   manager_id INT,
+  PRIMARY KEY (id),
   FOREIGN KEY (role_id) 
-  REFERENCES role_id(id),
+  REFERENCES role(id),
   FOREIGN KEY (manager_id)
   REFERENCES manager(id)
   ON DELETE SET NULL
 );
+CREATE TABLE role (
+  id INT NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(8,2),
+  department_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE SET NULL
+);                          
