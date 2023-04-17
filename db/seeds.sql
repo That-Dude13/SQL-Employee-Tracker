6eb6545978810d188a539d37abeb0f8b21b24a00
Change Agent <keasiley@icloud.com>
2023-04-17 13:14:57 -0500

    seeds edited, DB Updated

--- db/seeds.sql
+++ db/seeds.sql
@@ -18,12 +18,12 @@ VALUES (1, "Sales"),
 
     
 INSERT INTO employee (first_name, last_name, role_id, department_id, salary, manager_id)
-VALUES  ("John", "Doe", "Sales Lead", "Sales", 100000, null),
-        ("Mike", "Chan", "Sales person", "Sales", 100000, "John Doe" ),
-        ("Ashley", "Rodriguez", "Lead Engineer", "Engineering", 150000, null),
-        ("Kevin", "Tupik", "Software Engineer", "Engineering", 120000, "Ashley Rodriguez"),
-        ("Kunal", "Singh", "Account Manager", "Fianace", 160000, null),
-        ("Malia", "Brown", "Accountant", "Fianace", 125000, "Kunal Singh"),
-        ("Sarah", "Lourd", "Legal Team Lead", "Legal", 250000, null),
-        ("Tom", "Allen", "Lawyer", "Legal", 190000, "Sarah Lourd");
+VALUES  ("John", "Doe", "Sales Lead", 1, 100000, null),
+        ("Mike", "Chan", "Sales person", 1, 100000, "John Doe" ),
+        ("Ashley", "Rodriguez", "Lead Engineer", 2, 150000, null),
+        ("Kevin", "Tupik", "Software Engineer", 2, 120000, "Ashley Rodriguez"),
+        ("Kunal", "Singh", "Account Manager", 3, 160000, null),
+        ("Malia", "Brown", "Accountant", 3, 125000, "Kunal Singh"),
+        ("Sarah", "Lourd", "Legal Team Lead", 4, 250000, null),
+        ("Tom", "Allen", "Lawyer", 4, 190000, "Sarah Lourd");
         
