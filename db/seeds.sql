733226b290d19d8a68a37512ad7d42b9b0f72911
Change Agent <keasiley@icloud.com>
2023-04-17 11:46:23 -0500

    seeds edited, DB Updated

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,23 +4,24 @@ VALUES (1, "Sales"),
        (3, "Finance"),
        (4, "Legal"),
 
-INSERT INTO role (id, title, department_id, salary)
-VALUES  (1, "Sales Lead", "Sales", 100000),
-        (2, "Sales Person", "Sales", 80000),
-        (3, "Lead Engineer", "Engineering", 150000),
-        (4, "Software Engineer", "Engineering", 120000),
-        (5, "Account Manager", "Fianace", 160000), 
-        (6, "Accountant", "Fianance", 125000),  
-        (7, "Legal Team Lead", "Legal", 250000),
-        (8, "Lawyer", "Legal", 190000), 
+ INSERT INTO role (title, department_id, salary)
+ VALUES  ("Sales Lead", 1, 100000),
+        ("Sales Person", 1, 80000),
+        ("Lead Engineer", 2, 150000),
+        ("Software Engineer", 2, 120000),
+        ("Account Manager", 3, 160000),
+        ("Accountant", 3, 125000), 
+        ("Legal Team Lead", 4, 250000),
+        ("Lawyer", 4, 190000);
+
     
-INSERT INTO employee (id, first_name, last_name, title, department, salary, manager_id)
-VALUES  (1, "John", "Doe", "Sales Lead", "Sales", 100000, null),
-        (2, "Mike", "Chan", "Salesperson", "Sales", 100000, "John Doe" ),
-        (3, "Ashley", "Rodriguez", "Lead Engineer", "Engineering", 150000, null),
-        (4, "Kevin", "Tupik", "Software Engineer", "Engineering", 120000, "Ashley Rodriguez"),
-        (5, "Kunal", "Singh", "Account Manager", "Fianace", 160000, null),
-        (6, "Malia", "Brown", "Accountant", "Fianace", 125000, "Kunal Singh"),
-        (7, "Sarah", "Lourd", "Legal Team Lead", "Legal", 250000, null),
-        (8, "Tom", "Allen", "Lawyer", "Legal", 190000, "Sarah Lourd"),
+INSERT INTO employee (first_name, last_name, title, department, salary, manager_id)
+VALUES  ("John", "Doe", "Sales Lead", "Sales", 100000, null),
+        ("Mike", "Chan", "Sales person", "Sales", 100000, "John Doe" ),
+        ("Ashley", "Rodriguez", "Lead Engineer", "Engineering", 150000, null),
+        ("Kevin", "Tupik", "Software Engineer", "Engineering", 120000, "Ashley Rodriguez"),
+        ("Kunal", "Singh", "Account Manager", "Fianace", 160000, null),
+        ("Malia", "Brown", "Accountant", "Fianace", 125000, "Kunal Singh"),
+        ("Sarah", "Lourd", "Legal Team Lead", "Legal", 250000, null),
+        ("Tom", "Allen", "Lawyer", "Legal", 190000, "Sarah Lourd");
         
