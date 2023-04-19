7c3f0cf5e58f1b0d01dc2349694490e0228f7c02
Change Agent <keasiley@icloud.com>
2023-04-19 10:45:13 -0500

    test.js and query edited. More test added to both files.

--- db/test.js
+++ db/test.js
@@ -1,30 +1,35 @@
 // call once somewhere in the beginning of the app
-const cTable = require('console.table');
-require('dotenv').config()
-const mysql = require('mysql2');
+const cTable = require("console.table");
+require("dotenv").config();
+const mysql = require("mysql2");
 //Connect to database
-const db = mysql.createConnection(
-    {
-      host: 'localhost',
-      // MySQL username,
-      user: process.env.DB_USER,
-      // MySQL password
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-    },
-    console.log(`Connected to the employee_tracker database.`)
-);
+const db = mysql.createConnection({
+  host: "localhost",
+  // MySQL username,
+  user: process.env.DB_USER,
+  // MySQL password
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
+// Connect to database
+db.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected to the employee_tracker database.");
 
-cTable([
-   db.connect(function(err) {
-        if (err) throw err;
-        db.query("SELECT * FROM department", function (err, result, name) {
-          if (err) throw err;
-          //Return the fields object:
-          console.table(department);
-        });
-      
-    })
-    
-    ])
+  // Execute SELECT query
+  db.query("SELECT * FROM department", function (err, results) {
+    if (err) throw err;
+    console.table(results);
+  });
+
+  db.query("SELECT * FROM role", function (err, results) {
+    if (err) throw err;
+    console.table(results);
+  });
+
+  db.query("SELECT * FROM employee", function (err, results) {
+    if (err) throw err;
+    console.table(results);
+  });
+});
