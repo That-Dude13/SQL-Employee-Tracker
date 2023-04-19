893da0ebe5d7032f2a18c4b5628f80f964d987cf
Change Agent <keasiley@icloud.com>
2023-04-19 11:43:57 -0500

    test.js and query edited. More test added to both files.

--- db/query.sql
+++ db/query.sql
@@ -1,3 +1,14 @@
 SELECT *
 FROM role
 JOIN department ON department_id = role.department_id
+
+SELECT
+ role.title AS name, role_id
+FROM favorite_books
+JOIN book_prices ON favorite_books.book_price = book_prices.id;
+
+
+SELECT *
+FROM role
+JOIN role_id ON department_id = role.department_id
+
