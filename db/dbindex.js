9ee90be6c8735606b2f59fca8708b997c4be930a
Change Agent <keasiley@icloud.com>
2023-04-19 19:56:04 -0500

    test.js, and ignoreindex.js deleted. dbindex.js created in db folder. index.js created in main folder.

---
+++ db/dbindex.js
@@ -0,0 +1,60 @@
+const cTable = require('console.table');
+const inquirer = require("inquirer");
+require('dotenv').config()
+//get the client
+const mysql = require('mysql2');
+
+class EmployeeDB {
+    constructor() {
+        this.db = mysql.createConnection({
+            host: "localhost",
+            // Your username
+            user: process.env.DB_USER,
+            // Your password
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME
+        })
+
+        this.db.connect(function (err) {
+            if (err) throw err;
+        });
+    }
+
+    viewAllDepartments() {
+        const sql = `SELECT id, department_name AS title FROM department`;
+        return this.db.promise().query(sql);
+    }
+
+    viewAllRoles() {
+        const sql = `SELECT id, role_name AS title FROM role`;
+        return this.db.promise().query(sql);
+    }
+
+    viewAllEmployees() {
+        const sql = `SELECT id, first_name, last_name AS name FROM employee`;
+        return this.db.promise().query(sql);
+    }
+
+    addRoles(role_title, text) {
+        const sql = `INSERT INTO role role_title, review) VALUES (?, ?)`;
+        const params = [role_title, text];
+
+        return this.db.promise().query(sql,params);
+    }
+
+    addEmployee(first_name, last_name, text) {
+        const sql = `INSERT INTO employee first_name, last_name, role_id, manager_id, text) VALUES (?, ?, ?)`;
+        const params = [first_name, last_name, text];
+
+        return this.db.promise().query(sql,params);
+    }
+
+    updateEmployeeRole(employee, role_title, text ) {
+        const sql = `INSERT INTO employee role_title, text) VALUES (?, ?, ?)`;
+        const params = [employee, role_title, text];
+
+        return this.db.promise().query(sql,params);
+    }
+
+}
+module.exports = new EmployeeDB();
---
+++ index.js
@@ -0,0 +1,114 @@
+const cTable = require('console.table');
+const inquirer = require("inquirer");
+require('dotenv').config()
+//get the client
+const db = require("./db");
+
+
+async function userActionPrompt(){
+    const response = await inquirer.prompt([
+        {
+            type: "list",
+            name: "choice",
+            message: "What would you like to do?",
+            choices: [
+                {
+                    name: "View All Departments",
+                    value: "viewAllDepartments"
+                },
+                {
+                    name: "View All Roles",
+                    value: "viewAllRoles"
+                },
+                {
+                    name: "View All Employees",
+                    value: "viewAllEmployees"
+                },{
+                name: "Add a Role",
+                value: "addRole"
+            },
+                {
+                    name: "Add an Employee",
+                    value: "addEmployee"
+                },
+                {
+                    name: "Update Employee Role",
+                    value: "updateEmployeeRole"
+                },
+                {
+                    name: "Quit",
+                    value: "QUIT"
+                }
+            ]
+        }
+    ]);
+
+ 
+
+    switch(response.choice){
+        case "viewAllDepartments":
+            await viewAllDepartments();
+            break;
+        case "viewAllRoles":
+            await viewAllRoles();
+            break;
+        case "viewAllEmployees":
+            await viewAllEmployees();
+            break;
+            case "addRoles":
+                await addRoles();
+                break;
+            case "addEmployee":
+                await addEmployee();
+                break;
+            case "updateEmployeeRole":
+                await updateEmployeeRole();
+                break;
+        default:
+            console.log("Good Bye");
+            return;
+    }
+
+    await userActionPrompt();
+}
+
+async function viewAllDepartments(){
+    const [rows] = await db.viewAllDepartments();
+
+    console.table(rows);
+}
+
+
+
+async function viewAllRoles(){
+    const [rows] = await db.viewAllRoles();
+
+    console.table(rows);
+}
+
+async function viewAllEmployees(){
+    const [rows] = await db.viewAllEmployees();
+
+    console.table(rows);
+}
+
+async function addRoles(){
+    const [rows] = await db.addRole();
+
+    console.table(rows);
+}
+
+async function addEmployee(){
+    const [rows] = await db.addEmployee();
+
+    console.table(rows);
+}
+
+async function updateEmployeeRole(){
+    const [rows] = await db.updateEmployeeRole();
+
+    console.table(rows);
+}
+
+
+userActionPrompt();
